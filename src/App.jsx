4fe63927857c05cf10925527f364d16b495e4806
Anderson Marlon <anderson18.marlon@gmail.com>
2022-11-29 14:17:09 -0300

    Added: Theme Change

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import './App.css'
 
 function App() {
   const [value, setValue] = useState('# Hello World');
+  const [theme, setTheme] = useState('light');
 
   return (
     <div className="App">
       <h2>Note Markdown</h2>
 
-      <MDEditor height={200} value={value} onChange={setValue} />
+      <MDEditor height={200} value={value} onChange={setValue} data-color-mode={theme} />
 
       <small style={{ marginTop: '100px' }}>© 2022 - {new Date().getFullYear()} <a href="/">Note Markdown</a> - &nbsp;
         <a href="https://kalifyinc.vercel.app/" target="_blank" rel="noreferrer">Kalify Inc.</a></small>
+
+      <div className="theme" style={{ marginTop: '20px' }}>
+        <button onClick={() => setTheme('light')} style={{ marginRight: "20px" }}>🌕</button>
+        <button onClick={() => setTheme('dark')}>🌑</button>
+      </div>
     </div>
   )
 }
